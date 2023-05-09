d743b987f093dc0b69d9beaddda9e41d9d513b51
Bartosz Marcinkowski <bartosz.marcinkowski@mim.ai>
2023-05-09 12:20:50 +0200

    sphinx-rtd-theme

--- sphinx-docs/conf.py
+++ sphinx-docs/conf.py
@@ -32,5 +32,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
